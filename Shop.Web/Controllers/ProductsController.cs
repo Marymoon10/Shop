314e6935e08b0733e86214319b23c7762acf80fb
Maria Murillo <Maria.Murillo@evertecinc.com>
2021-12-21 03:54:12 +0100

    Primera parte web.net core

---
+++ Shop.Web/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Web.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly DataContext _context;
+        public ProductsController(DataContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Products.ToList());
+        }
+    }
+}
